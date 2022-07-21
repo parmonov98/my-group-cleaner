8d9a685a48df90d3ef8422d2f9bdcd00ebc756db
parmonov98 <parmonov98@yandex.ru>
2022-07-21 07:55:21 +0500

    files added

---
+++ start.php
@@ -0,0 +1,47 @@
+<?php
+
+use Zanzara\Zanzara;
+use Zanzara\Context;
+require __DIR__ . '/vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+$dotenv->required('TG_BOT_TOKEN');
+
+$bot = new Zanzara($_ENV['TG_BOT_TOKEN'] ?? "5374769262:AAFD_F0341iMnNf9kPYHMGiEQd6HwLP9-58");
+
+$bot->onCommand('start', function (Context $ctx) {
+    $ctx->sendMessage("Bu bot guruhdagi join va remove xabarlarni larni tozalaydi. Guruhga qo'shing va o'sha guruh adminligini botga bering. O'qish va O'chira olish huquqi bo'lishi kerak.");
+});
+
+$bot->onCommand('help', function (Context $ctx) {
+    $ctx->sendMessage("Bot yaratuvchi: @dasturchi_xizmati ");
+});
+
+$bot->onUpdate(function (Context $ctx) {
+
+    $update = $ctx->getUpdate();
+    $user = $update->getEffectiveUser();
+    $chat = $update->getEffectiveChat();
+    $message = $ctx->getMessage();
+
+    if (!$user->isBot()){
+        if ($message->getLeftChatMember() != null){
+            $ctx->deleteMessage($chat->getId(), $message->getMessageId());
+        }
+        if ($message->getNewChatMembers() != null){
+            $ctx->deleteMessage($chat->getId(), $message->getMessageId());
+        }
+    }
+});
+
+$bot->fallback(function(Context $ctx) {
+    $update = $ctx->getUpdate();
+    $user = $update->getEffectiveUser();
+    $chat = $update->getEffectiveChat();
+    if ($chat->getId() == $user->getId()){
+        $ctx->sendMessage("Berishingiz mumkin bo'lgan buyruqlar: /start, /help");
+    }
+});
+
+$bot->run();
